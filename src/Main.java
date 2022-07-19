149ef6843a5dd1f60a1146188c50c17dcb1b8a98
danielontivero <danielontivero@hotmail.com>
2022-07-19 14:51:23 -0300

    java mi proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var resultado = suma(10,30,40);
+        System.out.println(resultado);
+
+        Coche MiCoche = new Coche();
+        MiCoche.SumarPuerta();
+        MiCoche.SumarPuerta();
+        System.out.println(MiCoche.puerta);
+    }
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
+
+class Coche {
+    public int puerta = 1;
+
+    public void SumarPuerta(){
+        this.puerta++;
     }
 }
